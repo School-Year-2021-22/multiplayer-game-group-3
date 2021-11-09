f94ae68006cd9725050f756592af3c63b82c9c67
robigan <35210888+robigan@users.noreply.github.com>
2021-11-09 11:56:20 +0100

    Hello World

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/game/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 const gameRoomPrefix = 'room-'
