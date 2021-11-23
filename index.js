2ae79e829d4b862a175ce5f3dcc181e9897f2246
robigan <35210888+robigan@users.noreply.github.com>
2021-11-23 10:36:32 +0100

    Something is broken

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const port = process.env.PORT || 3000
+const { join } = require('path')
+// eslint-disable-next-line no-unused-vars
+const development = process.env.NODE_ENV === 'development'
+
 // var Datastore = require('nedb');
 // var rooms = new Datastore();
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
-app.use(express.static('public'));
-
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
+  res.sendFile(join(__dirname, '/views/index.ejs'))
+})
 
 const gameRoomPrefix = 'room-'
-
 const games = {
 }
+
 app.get('/rooms/:gameId', (req, res) => {
-  //Get room name
-  const roomName = gameRoomPrefix+req.params.gameId
+  // Get room name
+  const roomName = gameRoomPrefix + req.params.gameId
   // If the game doesn't exsist, I'll inizialize it
-  if (!games[roomName]){
+  if (!games[roomName]) {
     games[roomName] = {
       users: [],
       game: {
       }
     }
   }
-      
-  let userIndex = games[roomName].users.length
+
+  const userIndex = games[roomName].users.length
   // Connect the user to the socket
   io.on('connection', (socket) => {
     // Join the socket room
     socket.join(roomName)
     // If the user is new, add it
-    users = io.sockets.adapter.rooms.get(roomName);
-    let userIndex = 0
-    if (!games[roomName].users.find((u) => (u.id == socket.id))){
+    // eslint-disable-next-line no-undef
+    users = io.sockets.adapter.rooms.get(roomName)
+    // let userIndex = 0
+    if (!games[roomName].users.find((u) => (u.id === socket.id))) {
       const newUser = {
         id: socket.id,
         ready: false,
@@ -47,48 +51,47 @@ app.get('/rooms/:gameId', (req, res) => {
         left: parseInt(Math.random() * 200),
         click: 0
       }
-      
+
       games[roomName].users.push(newUser)
-      io.to(roomName).emit(roomName+'_update',games[roomName])
+      io.to(roomName).emit(roomName + '_update', games[roomName])
     }
 
     // When the user closes the session, i remove it from the game
-    socket.on("disconnect", () => {
+    socket.on('disconnect', () => {
       console.log('disconnect user', socket.id)
-      let i = 0
-      while (i < games[roomName].users.length) {
-        console.log(games[roomName].users[i].id);
-        if (games[roomName].users[i].id == socket.id){
-          games[roomName].users[i].online = false
-        }
-        i++;
-      } 
-      io.to(roomName).emit(roomName+'_update',games[roomName])
-    });
-    
+      // let i = 0
+      // while (i < games[roomName].users.length) {
+      //   console.log(games[roomName].users[i].id);
+      //   if (games[roomName].users[i].id == socket.id){
+      //     games[roomName].users[i].online = false
+      //   }
+      //   i++;
+      // }
+      const user = games[roomName].users.find(user => user.id === socket.id)
+      user ?? (games[roomName].users[user.id].online = false)
+      user ?? (io.to(roomName).emit(roomName + '_update', games[roomName]))
+    })
+
     // When we received a game update
     // We send it back to all the users
-    socket.on(roomName+'_user_update', gameUpdate => {
-      console.log('test',gameUpdate)
+    socket.on(roomName + '_user_update', gameUpdate => {
+      console.log('test', gameUpdate)
       games[roomName].users[gameUpdate.i] = {
         ...games[roomName].users[gameUpdate.i],
         ...gameUpdate.user
       }
 
-      io.to(roomName).emit(roomName+'_update',games[roomName])
-    });
-    
+      io.to(roomName).emit(roomName + '_update', games[roomName])
+    })
+  })
 
-  });
-
-  res.render('room',{
+  res.render('room', {
     room: roomName,
     userIndex: userIndex,
     game: games[roomName]
-  });
-
-});
+  })
+})
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
+  console.log(`Socket.IO server running at http://localhost:${port}/`)
+})
--- public/room.js
+++ public/room.js
@@ -1,15 +1,15 @@
 const app = {
-  data() {
+  data () {
     return {
       socket: null,
-      room: ROOM,
-      game: GAME,
-      userIndex: USER_INDEX
+      room: window.ROOM,
+      game: window.GAME,
+      userIndex: window.USER_INDEX
     }
   },
   methods: {
-    moveUser(amount) {
-      this.socket.emit(this.room+'_user_update', {
+    moveUser (amount) {
+      this.socket.emit(this.room + '_user_update', {
         i: this.userIndex,
         user: {
           left: this.game.users[this.userIndex].left + amount
@@ -17,14 +17,14 @@ const app = {
       })
     }
   },
-  mounted() {
-    this.socket = io();
-    // to send: 
-    // socket.emit('chat message', input.value);    
-    this.socket.on(ROOM+'_update', (msg) => {
+  mounted () {
+    this.socket = window.io()
+    // to send:
+    // socket.emit('chat message', input.value);
+    this.socket.on(window.ROOM + '_update', (msg) => {
       this.game = msg
-    });
+    })
   }
 }
 
-Vue.createApp(app).mount('.app')
+window.Vue.createApp(app).mount('.app')
