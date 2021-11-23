bf2756efc9f6706e9a32a0e788a4f1e01e2c0624
robigan <35210888+robigan@users.noreply.github.com>
2021-11-23 11:33:02 +0100

    Bono

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.get('/homepage', (req, res) => {
   res.render('homepage')
 })
 
