50740c2330979c0447898cf7c1c31e53bea603c1
robigan <35210888+robigan@users.noreply.github.com>
2022-01-12 14:07:47 +0100

    Sync local changes to remote

--- public/room.js
+++ public/room.js
@@ -1,29 +1,12 @@
 const app = {
   data () {
     return {
-      socket: null,
-      room: window.ROOM,
-      game: window.GAME,
-      userIndex: window.USER_INDEX
+      count: 0
     }
   },
   methods: {
-    moveUser (amount) {
-      this.socket.emit(this.room + '_user_update', {
-        i: this.userIndex,
-        user: {
-          left: this.game.users[this.userIndex].left + amount
-        }
-      })
-    }
   },
   mounted () {
-    this.socket = window.io()
-    // to send:
-    // socket.emit('chat message', input.value);
-    this.socket.on(window.ROOM + '_update', (msg) => {
-      this.game = msg
-    })
   }
 }
 
