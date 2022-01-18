5661c9b44cf06b556435cb6ef30cdba57bc472dc
robigan <35210888+robigan@users.noreply.github.com>
2022-01-18 11:42:11 +0100

    Server Side Logic changes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const { Server, Socket } = require('socket.io')
+/**
+ * @type {import("http").Server}
+ */
 const http = require('http').Server(app)
-const io = require('socket.io')(http)
+const io = Server(http)
 const port = process.env.PORT || 3000
 // eslint-disable-next-line no-unused-vars
 // const development = process.env.NODE_ENV === 'development'
@@ -16,82 +20,112 @@ app.get('/', (req, res) => {
   res.render('homepage')
 })
 
-// app.get('/rooms/:gameId', (req, res) => {
-//   res.render('room')
-// })
-
-const gameRoomPrefix = 'room-'
-const games = {
-}
-app.get('/rooms/:gameId/:name', (req, res) => {
-  // Get room name
-  const roomName = gameRoomPrefix + req.params.gameId
-  // If the game doesn't exsist, I'll inizialize it
-  if (!games[roomName]) {
-    games[roomName] = {
-      users: [],
+const games = {}
+app.get('/rooms/:gameId', (req, res) => {
+  const gameID = req.params.gameId
+  if (!games[gameID]) {
+    games[gameID] = {
       game: {
+        counter: 0
       }
     }
   }
 
-  const userIndex = games[roomName].users.length
+  // For security reasons we shall refrence the roomObj using a special var to avoid TOCTOU attacks
+  const roomObj = games[gameID];
 
-  const newUser = {
-    id: userIndex,
-    ready: false,
-    online: true,
-    name: req.params.name,
-    cuttedFruits: 0,
-    left: parseInt(Math.random() * 200),
+  /**
+   * Socket handler logic that will handle counter updates and server io room join and leave
+   * @param {Socket} socket
+   */
+  const socketHandler = (socket) => {
+    socket.join(gameID)
+    socket.on('disconnect', () => socket.leave(gameID))
+    socket.on('_cut_fruit', () => {
+      roomObj.game.counter++;
+      io.to(gameID).emit('_counter_update', roomObj.game.counter)
+    })
+    socket.emit('_counter_update', roomObj.game.counter)
   }
+  io.on('connection', socketHandler)
 
-  games[roomName].users.push(newUser)
-
-  // Connect the user to the socket
-  io.on('connection', (socket) => {
-    games[roomName].users[games[roomName].users.length - 1].session_id = socket.id
-    io.to(roomName).emit(roomName + '_update', games[roomName])
-    // Join the socket room
-    socket.join(roomName)
-    // If the user is new, add it
-    // users = io.sockets.adapter.rooms.get(roomName);
-    // When the user closes the session, i remove it from the game
-    socket.on('disconnect', () => {
-      console.log('disconnect user', socket.id)
-      let i = 0
-      while (i < games[roomName].users.length) {
-        console.log(games[roomName].users[i].session_id);
-        if (games[roomName].users[i].session_id == socket.id){
-          games[roomName].users[i].online = false
-        }
-        i++;
-      }
-      // const user = games[roomName].users.find(user => user.id === socket.id)
-      // user ?? (games[roomName].users[user.id].online = false)
-      // user ?? (io.to(roomName).emit(roomName + '_update', games[roomName]))
-      io.to(roomName).emit(roomName + '_update', games[roomName])
-    })
-
-    // When we received a game update
-    // We send it back to all the users
-    socket.on(roomName + '_user_update', gameUpdate => {
-      console.log('test', gameUpdate)
-      games[roomName].users[gameUpdate.i] = {
-        ...games[roomName].users[gameUpdate.i],
-        ...gameUpdate.user
-      }
-
-      io.to(roomName).emit(roomName + '_update', games[roomName])
-    })
-  })
-
-  res.render('room', {
-    room: roomName,
-    userIndex: userIndex,
-    game: games[roomName]
-  })
+  res.render('room')
 })
+
+// WE HAVE DECIDED TO DO SINGLEPLAYER
+
+// const gameRoomPrefix = 'room-'
+// const games = {
+// }
+// app.get('/rooms/:gameId/:name', (req, res) => {
+//   // Get room name
+//   const roomName = gameRoomPrefix + req.params.gameId
+//   // If the game doesn't exsist, I'll inizialize it
+//   if (!games[roomName]) {
+//     games[roomName] = {
+//       users: [],
+//       game: {
+//       }
+//     }
+//   }
+
+//   const userIndex = games[roomName].users.length
+
+//   const newUser = {
+//     id: userIndex,
+//     ready: false,
+//     online: true,
+//     name: req.params.name,
+//     cuttedFruits: 0,
+//     left: parseInt(Math.random() * 200),
+//   }
+
+//   games[roomName].users.push(newUser)
+
+//   // Connect the user to the socket
+//   io.on('connection', (socket) => {
+//     games[roomName].users[games[roomName].users.length - 1].session_id = socket.id
+//     io.to(roomName).emit(roomName + '_update', games[roomName])
+//     // Join the socket room
+//     socket.join(roomName)
+//     // If the user is new, add it
+//     // users = io.sockets.adapter.rooms.get(roomName);
+//     // When the user closes the session, i remove it from the game
+//     socket.on('disconnect', () => {
+//       console.log('disconnect user', socket.id)
+//       let i = 0
+//       while (i < games[roomName].users.length) {
+//         console.log(games[roomName].users[i].session_id);
+//         if (games[roomName].users[i].session_id == socket.id){
+//           games[roomName].users[i].online = false
+//         }
+//         i++;
+//       }
+//       // const user = games[roomName].users.find(user => user.id === socket.id)
+//       // user ?? (games[roomName].users[user.id].online = false)
+//       // user ?? (io.to(roomName).emit(roomName + '_update', games[roomName]))
+//       io.to(roomName).emit(roomName + '_update', games[roomName])
+//     })
+
+//     // When we received a game update
+//     // We send it back to all the users
+//     socket.on(roomName + '_user_update', gameUpdate => {
+//       console.log('test', gameUpdate)
+//       games[roomName].users[gameUpdate.i] = {
+//         ...games[roomName].users[gameUpdate.i],
+//         ...gameUpdate.user
+//       }
+
+//       io.to(roomName).emit(roomName + '_update', games[roomName])
+//     })
+//   })
+
+//   res.render('room', {
+//     room: roomName,
+//     userIndex: userIndex,
+//     game: games[roomName]
+//   })
+// })
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`)
--- public/room.js
+++ public/room.js
@@ -2,36 +2,27 @@ const app = {
   data () {
     return {
       socket: null,
-      room: ROOM, //eslint-disable-line
-      game: GAME, //eslint-disable-line
-      userIndex: USER_INDEX //eslint-disable-line
+      counter: 0
+      // room: ROOM, //eslint-disable-line
+      // game: GAME, //eslint-disable-line
+      // userIndex: USER_INDEX //eslint-disable-line
     }
   },
   methods: {
-    moveUser (amount) {
-      this.socket.emit(this.room + '_user_update', {
-        i: this.userIndex,
-        user: {
-          left: this.game.users[this.userIndex].left + amount
-        }
-      })
-    },
-    cutFruit (amount) {
-      this.socket.emit(this.room + '_user_update', {
-        i: this.userIndex,
-        user: {
-          cuttedFruits: this.game.users[this.userIndex].cuttedFruits + 1
-        }
-      })
+    cutFruit () {
+      this.socket.emit('_cut_fruit')
     }
   },
   mounted () {
     this.socket = window.io()
     // to send:
     // socket.emit('chat message', input.value);
-    this.socket.on(ROOM + '_update', (msg) => {
-      console.log(msg)
-      this.game = msg
+    // this.socket.on(ROOM + '_update', (msg) => { //eslint-disable-line
+    //   console.log(msg)
+    //   this.game = msg
+    // })
+    this.socket.on('_counter_update', (serverCounter) => {
+      this.counter = serverCounter;
     })
   }
 }
